898380e2c45b1b5dd5a26d28058a80218c98b8a7
Pablo Hernandez-Ferreiros <pablohf@programgeo.net>
2022-08-29 16:55:52 +0200

    added aws_ipcheck.sh

---
+++ aws_ipcheck.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+
+curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4
